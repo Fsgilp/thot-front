582fa9c2268b6e98e81d13533be58180535db3ba
Fernando Silvano Gil Pérez <fsgil@indra.es>
2022-06-19 14:52:16 +0200

    Footer y nuevos enlaces del menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { StorageService } from './services/storage.service';
+import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,11 +9,48 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'Thot Application';
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showUserBoard = false;
+  showModeratorBoard = false;
+  username?: string;
 
   constructor(
     private translate: TranslateService,
+    private authService: AuthService,
+    private storageService: StorageService
   ) {
     translate.setDefaultLang('es');
     translate.use('es');
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showUserBoard = this.roles.includes('ROLE_USER');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+
+    window.location.reload();
   }
 }
