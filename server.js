6418f90e5e32211f9553cabc86b718c2034d7b61
Fernando Silvano Gil Pérez <fsgil@indra.es>
2022-09-18 23:26:58 +0200

    Modifiación por ruta del dist

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/thot-angular'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/thot-angular/index.html'));});
+app.use(express.static(__dirname + '/dist/Thot'));
+app.get('*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/Thot/index.html'));});
+/*app.get('*', (req, res) => {
+  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});*/
+
 app.listen(process.env.PORT || 8082);
