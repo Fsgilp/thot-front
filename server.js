227480df44e72607dd20f54c8647167a57c499e6
Fernando Silvano Gil Pérez <fsgil@indra.es>
2022-09-18 22:44:47 +0200

    Modificaciones con buena pinta

--- server.js
+++ server.js
@@ -1,32 +1,7 @@
-// Use Express
-var express = require("express");
-// Use body-parser
-var bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// Create new instance of the express server
-var app = express();
-
-// Define the JSON parser as a default way
-// to consume and produce data through the
-// exposed APIs
-app.use(bodyParser.json());
-
-// Create link to Angular build directory
-// The `ng build` command will save the result
-// under the `dist` folder.
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-
-// Init the server
-var server = app.listen(process.env.PORT || 4200, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-});
-
-/*  "/api/status"
- *   GET: Get server status
- *   PS: it's just an example, not mandatory
- */
-app.get("/api/status", function (req, res) {
-    res.status(200).json({ status: "UP" });
-});
+app.use(express.static(__dirname + '/dist/<app-name>'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));});
+app.listen(process.env.PORT || 8082);
